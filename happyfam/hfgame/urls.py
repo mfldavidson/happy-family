0c13c814b9512703818e3a386ad5a84350e817ca
Maggie Davidson <jmagdavidson@gmail.com>
2019-10-31 10:57:48 -0400

    Add custom form validation, post to game_detail instead of game_status

--- happyfam/hfgame/urls.py
+++ happyfam/hfgame/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('game/<int:pk>/', views.GameDetailView.as_view(), name='game_detail'),
     path('game/create/', views.GameCreateView.as_view(), name='game_create'),
     path('game/<int:pk>/update/', views.GameUpdateView.as_view(), name='game_update'),
-    path('game/<int:pk>/status/', views.GameStatusView.as_view(), name='game_status'),
     path('game/<int:pk>/delete/', views.GameDeleteView.as_view(), name='game_delete'),
-    path('game/<int:pk>/name', views.NameCreateView.as_view(), name='name_create'),
-    path('name/<int:pk>/delete', views.NameDeleteView.as_view(success_url=reverse_lazy('agame_list')), name='name_delete'),
+    path('game/<int:pk>/name/', views.NameCreateView.as_view(), name='name_create'),
+    path('name/<int:pk>/delete/', views.NameDeleteView.as_view(success_url=reverse_lazy('agame_list')), name='name_delete'),
 ]
--- happyfam/hfgame/views.py
+++ happyfam/hfgame/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.conf import settings
 from hfgame.models import *
 from hfgame.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerDeleteView
 from hfgame.forms import *
@@ -16,51 +17,88 @@ class AGamesListView(OwnerListView):
 
     def get(self, request) :
         game_list = Game.objects.filter(status=1).order_by('-updated_at').all()
-        ctx = {'game_list': game_list}
-        return render(request, self.template_name, ctx)
+        context = {'game_list': game_list}
+        return render(request, self.template_name, context)
 
-class GameDetailView(OwnerDetailView):
+def get_game_detail_context_data(game, user, status_form):
+    ''' Function to get the context data necessary for GameDetailView that stays the same between GET and POST '''
+
+    names = list(Name.objects.filter(game=game).all())
+    shuffle(names)
+    name_form = NameForm()
+    name_exists = Name.objects.filter(game=game, owner=user).first()
+    context = {
+        'game': game,
+        'status_form': status_form,
+        'names': names,
+        'name_form': name_form,
+        'name_exists': name_exists
+        }
+    return context
+
+class GameDetailView(View):
     model = Game
     template_name = 'game_detail.html'
 
     def get(self, request, pk):
         game = Game.objects.get(id=pk)
         status_form = GameStatusForm(instance=game)
-        names = list(Name.objects.filter(game=game).all())
-        shuffle(names)
-        name_form = NameForm()
-        name_exists = Name.objects.filter(game=game, owner=self.request.user).first()
-        three_hrs_ago = datetime.datetime.today()-datetime.timedelta(hours=3)
-        context = {
-            'game': game,
-            'status_form': status_form,
-            'names': names,
-            'name_form': name_form,
-            'name_exists': name_exists,
-            'three_hrs_ago': three_hrs_ago
-            }
+        context = get_game_detail_context_data(game, self.request.user, status_form)
         return render(request, self.template_name, context)
+
+    def post(self, request, pk=None) :
+        game = Game.objects.get(id=pk)
+
+        status_form = GameStatusForm(self.request.POST, instance=game)
+
+        # check if the form is valid using the custom validation in GameStatusForm.clean which checks if the winner has been set for In Play games before we change the status to Done, and return an error to the user if no winner is selected
+        if not status_form.is_valid() :
+            context = get_game_detail_context_data(game, self.request.user, status_form)
+            return render(request, self.template_name, context)
+
+        else:
+            # get each of the GameStatus objects
+            accepting_names = GameStatus.objects.get(id=1)
+            in_play = GameStatus.objects.get(id=2)
+            done = GameStatus.objects.get(id=3)
+
+            # get the staged game object
+            g = status_form.save(commit=False)
+
+            # advance the staged game object's status to the next status
+            if g.status == accepting_names:
+                g.status = in_play
+            elif g.status == in_play:
+                g.status = done
+            else:
+                g.status = accepting_names
+
+            # save the form and the many to many winner relationship
+            status_form.save()
+            status_form._save_m2m()
+
+            return redirect(reverse_lazy('game_detail', args=[pk]))
 
 class GameCreateView(LoginRequiredMixin, View):
     template = 'hfgame/game_form.html'
     success_url = reverse_lazy('agame_list')
+
     def get(self, request, pk=None) :
         form = GameCreateForm()
-        ctx = { 'form': form }
-        return render(request, self.template, ctx)
+        context = { 'form': form }
+        return render(request, self.template, context)
 
     def post(self, request, pk=None) :
         form = GameCreateForm(request.POST, request.FILES or None)
 
         if not form.is_valid() :
-            ctx = {'form' : form}
-            return render(request, self.template, ctx)
+            context = {'form' : form}
+            return render(request, self.template, context)
 
         # Add creator and owner to the model before saving
         f = form.save(commit=False)
-        print(self.request.user)
         f.owner = self.request.user
-        # f.creator = self.request.user
+        f.creator = self.request.user
         f.save()
         return redirect(self.success_url)
 
@@ -71,37 +109,22 @@ class GameUpdateView(LoginRequiredMixin, View):
     def get(self, request, pk) :
         g = get_object_or_404(Game, id=pk, owner=self.request.user)
         form = GameUpdateForm(instance=g)
-        ctx = { 'form': form }
-        return render(request, self.template, ctx)
+        context = { 'form': form }
+        return render(request, self.template, context)
 
     def post(self, request, pk=None) :
         g = get_object_or_404(Game, id=pk, owner=self.request.user)
         form = GameUpdateForm(request.POST, request.FILES or None, instance=g)
+        print(request.POST)
 
         if not form.is_valid() :
-            ctx = {'form' : form}
-            return render(request, self.template, ctx)
+            context = {'form' : form}
+            return render(request, self.template, context)
 
         g = form.save(commit=False)
         g.save()
 
         return redirect(self.success_url)
-
-class GameStatusView(LoginRequiredMixin, View):
-    template = 'hfgame/game_detail.html'
-
-    def post(self, request, pk=None) :
-        g = get_object_or_404(Game, id=pk, owner=self.request.user)
-        form = GameStatusForm(request.POST, request.FILES or None, instance=g)
-
-        if not form.is_valid() :
-            ctx = {'form' : form}
-            return render(request, self.template, ctx)
-
-        g = form.save(commit=False)
-        g.save()
-
-        return redirect((reverse_lazy('game_detail', args=[pk])))
 
 class GameDeleteView(OwnerDeleteView):
     model = Game
